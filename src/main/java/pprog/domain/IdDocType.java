97bcc778aa3e186ef12ce3a050d03a7482c0e8f4
Daniel André Freitas Duarte Silva <1231046@isep.ipp.pt>
2024-04-29 00:08:03 +0100

    ED-20: US6 - corrections in implementation

--- src/main/java/pprog/domain/IdDocType.java
+++ src/main/java/pprog/domain/IdDocType.java
@@ -1,6 +1,11 @@
 package pprog.domain;
 
 public enum IdDocType {
-    TAXPAYER_NUMBER, CITIZEN_CARD, PASSPORT
+    TAXPAYER_NUMBER, CITIZEN_NUMBER, PASSPORT;
+        @Override
+        public String toString() {
+            return name().charAt(0) + name().substring(1).toLowerCase().replace('_', ' ');
+        }
 }
+
 
