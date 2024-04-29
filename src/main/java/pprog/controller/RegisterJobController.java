1e854fe84d5c8898c2823abfccb1c5a7709694c8
Samara Miranda <samara23.miranda@hotmail.com>
2024-04-29 16:19:25 +0100

    ED-16 implementation of the Controller and UI

--- src/main/java/pprog/controller/RegisterJobController.java
+++ src/main/java/pprog/controller/RegisterJobController.java
@@ -1,8 +1,22 @@
 package pprog.controller;
 
+import pprog.domain.Job;
+import pprog.repository.JobRepository;
+import pprog.repository.Repositories;
+
 public class RegisterJobController {
 
-    public RegisterJobController () {
+    private JobRepository jobRepository;
 
+    // Método para registrar um novo cargo
+    public void registerJob(String name, String description) {
+        // Obter instância do repositório de cargos
+        jobRepository = Repositories.getInstance().getJobRepository();
+
+        // Criar um novo objeto de cargo
+        Job job = new Job(name, description);
+
+        // Adicionar o cargo ao repositório
+        jobRepository.addJob(job);
     }
 }
