6dc30c330477fff6a79d4c14d101106b02d8ce27
Igor Coutinho <1230543@isep.ipp.pt>
2024-05-21 10:45:07 +0100

    pprog - RegisterGreenSpaceController

--- src/main/java/pprog/controller/RegisterGreenSpaceController.java
+++ src/main/java/pprog/controller/RegisterGreenSpaceController.java
@@ -1,4 +1,39 @@
 package pprog.controller;
 
+import pprog.domain.GreenSpace;
+import pprog.repository.Repositories;
+import pprog.repository.VehicleRepository;
+
+import java.util.Date;
+
+
 public class RegisterGreenSpaceController {
+
+    private GreenSpaceRepository GreenSpaceRepository;
+
+    public RegisterGreenSpaceController() {
+        getGreenSpaceRepository();
+    }
+
+    public RegisterGreenSpaceController(GreenSpaceRepository GreenSpaceRepository) {
+        this.GreenSpaceRepository = GreenSpaceRepository;
+    }
+
+    private VehicleRepository getVehicleRepository() {
+        if (GreenSpaceRepository == null) {
+            Repositories repositories = Repositories.getInstance();
+            GreenSpaceRepository = repositories.getVehicleRepository();
+        }
+        return GreenSpaceRepository;
+    }
+
+    public boolean registerGreenSpace(String name, String address, int type, double area) {
+        try {
+            getVehicleRepository().registerVehicle(name, address, type, area);
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.out.println("\n" + e.getMessage());
+            return false;
+        }
+    }
 }
