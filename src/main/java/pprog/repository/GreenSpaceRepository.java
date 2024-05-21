05ad85493138803d4d909035602c61678d158a10
Igor Coutinho <1230543@isep.ipp.pt>
2024-05-21 10:55:34 +0100

    pprog - RegisterGreenSpaceRepository

--- src/main/java/pprog/repository/GreenSpaceRepository.java
+++ src/main/java/pprog/repository/GreenSpaceRepository.java
@@ -1,4 +1,48 @@
 package pprog.repository;
 
+import pprog.domain.GreenSpace;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class GreenSpaceRepository {
+
+    private final List<GreenSpace> greenSpacesList;
+
+    public GreenSpaceRepository() {
+        greenSpacesList = new ArrayList<>();
+    }
+
+    public GreenSpace registerGreenSpace (String name, String address, int type, double area) {
+        GreenSpace newGreenSpace = null;
+        GreenSpace greenSpace = new GreenSpace (name, address, type, area);
+        if (addGreenSpace(greenSpace)) {
+            newGreenSpace = greenSpace;
+        }
+        return newGreenSpace;
+    }
+
+    private boolean addGreenSpace (GreenSpace greenSpace) {
+        if (validateGreenSpace(greenSpace)) {
+            greenSpacesList.add(greenSpace);
+            return true;
+        } else {
+            throw new IllegalArgumentException("Green space already exists in the repository");
+        }
+    }
+
+    private boolean validateGreenSpace (GreenSpace greenSpace) {
+        return !greenSpacesList.contains(greenSpace);
+    }
+
+    public List<GreenSpace> getGreenSpacesList() {
+        return greenSpacesList;
+    }
+
+    @Override
+    public String toString() {
+        return "Green space=" + greenSpacesList + '}';
+    }
 }
+
