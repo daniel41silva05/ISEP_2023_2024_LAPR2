6e28de1774f0224d1eeba6a3388374720a53e8ed
Rafael Barbosa <1230544@isep.ipp.pt>
2024-05-25 19:34:23 +0100

    pprog changes US21 #27

--- src/main/java/pprog/ui/classesUI/AddTaskToDoListUI.java
+++ src/main/java/pprog/ui/classesUI/AddTaskToDoListUI.java
@@ -12,9 +12,9 @@ public class AddTaskToDoListUI {
     private final AddTaskToDoListController controller;
     private String title;
     private String description;
-    private EmergencyDegree degreeOfUrgency;
+    private int degreeOfUrgency;
     private int expectedDuration;
-    private TaskType type;
+    private int type;
     private GreenSpace greenSpace;
 
     public AddTaskToDoListUI(AddTaskToDoListController controller) {
@@ -43,4 +43,65 @@ public class AddTaskToDoListUI {
         } while (!title.matches("[a-zA-Z ]+}"));
         return title;
     }
+
+    private String requestDescription() {
+        Scanner input = new Scanner(System.in);
+        String description;
+        do {
+            System.out.println("Description: ");
+            description = input.nextLine().trim();
+            if (!description.matches("[a-zA-Z ]+")) {
+                System.out.println("Invalid title. Please enter a valid description.");
+            }
+        } while (!description.matches("[a-zA-Z ]+}"));
+        return description;
+    }
+
+    private int requestDegreeOfUrgency() {
+        Scanner input = new Scanner(System.in);
+        int degreeOfUrgency;
+        System.out.println("Degree of Urgency: ");
+        System.out.println("1 - HIGH");
+        System.out.println("2 - MEDIUM");
+        System.out.println("3 - LOW");
+        degreeOfUrgency = input.nextInt();
+
+        if (degreeOfUrgency < 1 || degreeOfUrgency > 3) {
+            System.out.println("Invalid input. Please choose a valid option (1, 2 or 3).");
+            return requestDegreeOfUrgency();
+        } else {
+            return degreeOfUrgency;
+        }
+    }
+
+    private int requestExpectedDuration() {
+        Scanner input = new Scanner(System.in);
+        int expectedDuration;
+        System.out.println("Expected Duration: ");
+        expectedDuration = input.nextInt();
+
+        if (expectedDuration < 0) {
+            System.out.println("Invalid input. Please insert a valid duration (greater than 0).");
+            return requestExpectedDuration();
+        } else {
+            return expectedDuration;
+        }
+    }
+
+    private int requestType() {
+        Scanner input = new Scanner(System.in);
+        int type;
+        System.out.println("Type: ");
+        System.out.println("1 - REGULAR");
+        System.out.println("2 - OCCASIONAL");
+        type = input.nextInt();
+
+        if (type < 1 || type > 2) {
+            System.out.println("Invalid input. Please choose a valid option (1 or 2).");
+            return requestType();
+        } else {
+            return type;
+        }
+    }
 }
+
