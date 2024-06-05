89176c90ece3e09c606a5c0cc2e8a5e47e01b432
Samara Miranda <1230432@isep.ipp.pt>
2024-06-05 09:34:58 +0100

    Applying JavaDoc Comments

--- src/main/java/pprog/domain/Algoritmo2.java
+++ src/main/java/pprog/domain/Algoritmo2.java
@@ -4,17 +4,31 @@ import pprog.interfaces.SortingAlgorithm;
 
 import java.util.List;
 
+/**
+ * Implementation of a sorting algorithm that sorts a list of green spaces by their area in descending order
+ * using the bubble sort algorithm.
+ */
 public class Algoritmo2 implements SortingAlgorithm {
 
+    /**
+     * Sorts the given list of managed green spaces.
+     *
+     * @param managedGreenSpacesList the list of green spaces to be sorted
+     */
     @Override
     public void sort(List<GreenSpace> managedGreenSpacesList) {
         sortList(managedGreenSpacesList);
     }
 
+    /**
+     * Sorts the list of managed green spaces by their area in descending order using the bubble sort algorithm.
+     *
+     * @param managedGreenSpacesList the list of green spaces to be sorted
+     */
     private void sortList(List<GreenSpace> managedGreenSpacesList) {
         boolean swapped;
 
-        for (int i = 0; i < managedGreenSpacesList.size()- 1; i++) {
+        for (int i = 0; i < managedGreenSpacesList.size() - 1; i++) {
             swapped = false;
             for (int j = 0; j < managedGreenSpacesList.size() - i - 1; j++) {
                 if (managedGreenSpacesList.get(j).getArea() < managedGreenSpacesList.get(j + 1).getArea()) {
